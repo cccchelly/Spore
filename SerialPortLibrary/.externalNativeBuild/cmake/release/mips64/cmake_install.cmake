9c4d085e84629d2d1a0c326c7fcc386c6c7e7532
liuxiangdong <liuxiangdong@chinazhix.com>
2019-10-09 14:08:23 +0800

    first commit

--- SerialPortLibrary/.externalNativeBuild/cmake/release/mips64/cmake_install.cmake
+++ SerialPortLibrary/.externalNativeBuild/cmake/release/mips64/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/apple/Documents/project/WitAgN/SerialPortLibrary/src/main/cpp
+# Install script for directory: /Users/apple/Documents/project/Spore/SerialPortLibrary/src/main/cpp
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/apple/Documents/project/WitAgN/SerialPortLibrary/.externalNativeBuild/cmake/release/mips64/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/apple/Documents/project/Spore/SerialPortLibrary/.externalNativeBuild/cmake/release/mips64/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
